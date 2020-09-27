97e162d07e7f5f7038b9f937274206d3af176346
sherwinp <sherwinprice@hotmail.com>
2020-09-27 11:53:24 -0400

    handle secured session

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ from starlette.responses import FileResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 async def homepage(request):
+    auth_header = None
+    try:
+      auth_header = request.headers["Authorization"]
+    except KeyError:
+      pass
+    if auth_header:
+        return FileResponse('dist/secured.html')
     return FileResponse('dist/index.html')
 
 async def auth(request):
-    return JSONResponse({'token': 'world'})
+    result = 'world';
+    auth_header = request.headers["Authorization"]
+    if auth_header:
+        result = 'known';
+    return JSONResponse({'token': result })
 
 routes = [
     Route('/auth', auth),
